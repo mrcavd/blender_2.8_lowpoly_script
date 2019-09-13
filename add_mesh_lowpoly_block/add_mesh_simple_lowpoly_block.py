49b75d8512d2e4aa4097f7d10ca47d3980ed08e8
mrcavd <ca_vindeng@icloud.com>
2019-09-12 19:48:11 -0700

    add new feature
    right click on existing lowpoly block to create a new block

--- add_mesh_lowpoly_block/add_mesh_simple_lowpoly_block.py
+++ add_mesh_lowpoly_block/add_mesh_simple_lowpoly_block.py
@@ -1,13 +1,14 @@
 import bpy, bmesh
+from bpy.types import Operator
 import random, math
 from bpy.props import (IntProperty, BoolProperty, FloatProperty)
 
 
-def add_mesh(self, context):
+def add_mesh(dis, x, y, z, i, context):
 
 	#add mesh and create group
-	bpy.ops.mesh.primitive_cube_add(enter_editmode=False, location=(0, 0, 1))
-	#bpy.data.objects["Cube"].name = 'simple_poly'
+	bpy.ops.mesh.primitive_cube_add(enter_editmode=False, location=(0, i*2, 0))
+	bpy.data.objects["Cube"].name = 'simple_poly'
 	obj = context.object
 	obj.name = 'simple_poly'
 	me = obj.data
@@ -28,15 +29,15 @@ def add_mesh(self, context):
 		# to optimize viewing on YZ plane
 		z_modifier = 0.5
 	
-		if(self.x == 1):
+		if(x == 1):
 			while t_mod_x < 0.04 and t_mod_x > -0.04:
-				t_mod_x = random.uniform(-self.dis, self.dis)
-		if(self.y == 1):
+				t_mod_x = random.uniform(-dis, dis)
+		if(y == 1):
 			while t_mod_y < 0.04 and t_mod_y > -0.04:
-				t_mod_y = random.uniform(-self.dis, self.dis)
-		if(self.z == 1):
+				t_mod_y = random.uniform(-dis, dis)
+		if(z == 1):
 			while t_mod_z < 0.04 and t_mod_z > -0.04:
-				t_mod_z = random.uniform(-self.dis, self.dis)
+				t_mod_z = random.uniform(-dis, dis)
 		bpy.ops.transform.translate(value=(t_mod_x, t_mod_y , t_mod_z*z_modifier), \
 									orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), \
 									orient_matrix_type='GLOBAL', mirror=True, \
@@ -49,11 +50,11 @@ def add_mesh(self, context):
 	bpy.ops.object.modifier_add(type='TRIANGULATE')
 	bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Triangulate")
 	
-	#return {'FINISHED'}
+	return obj
 
 	
 
-class AddLowPolyBlock(bpy.types.Operator):
+class AddLowPolyBlock(Operator):
 	bl_idname = "mesh.simple_low_poly_block_add"
 	bl_label = "Add Simple Low Poly Block"
 	bl_description = "Add simple lowploy block to scene"
@@ -61,42 +62,65 @@ class AddLowPolyBlock(bpy.types.Operator):
 
 	dis: FloatProperty(
 		name = "Displacement Range",
-		description = "Range of how much each vertices shift on respected axis in both "
-						"positive and negative direction",
+		description = "Range of how much each vertices shift on respected axis in both positive and negative direction",
 		min = 0.09,
 		max = 0.50,
-		default = 0.18
-	)
+		unit = 'LENGTH',
+		default = 0.18		
+		)
 
 	number_of_blocks: IntProperty(
 		name = "Number of Block",
 		description = "Number of Block generate at once on Y axis",
 		min = 1,
 		max = 10,
-		default = 2
-	)
+		default = 1
+		)
+
 	x: BoolProperty(
-		name = "X axis",
+		name = "Enable X Axis Lowpoly Effect",
 		description = "Enable vertex dispalcement in X axis",
 		default = True
-	)
+		)
 	y: BoolProperty(
-		name = "Y axis",
+		name = "Enable Y Axis Lowpoly Effect",
 		description = "Enable vertex dispalcement in Y axis",
 		default = False
-	)
+		)
 	z: BoolProperty(
-		name = "Z axis",
+		name = "Enable Z Axis Lowpoly Effect",
 		description = "Enable vertex dispalcement in Z axis",
 		default = False
-	)
+		)
+
+	#def draw(self, context):
+	#	layout = self.layout
+	#
+	#	box = layout.box()
+	#	box.prop(self, 'dis')
+	#
+	#	box = layout.box()
+	#	box.prop(self, 'x')
+	#	box.prop(self, 'y')
+	#	box.prop(self, 'z')
 
 
 	def execute(self, context):
 		
-		add_mesh(self, context)
+		for i in range(self.number_of_blocks):
+			obj = add_mesh(self.dis, self.x, self.y, self.z, i, context)
+		
+		#obj["number_of_blocks"] = self.number_of_blocks
+		#obj["dis"] = self.dis
+		#obj["x"] = self.x
+		#obj["y"] = self.y
+		#obj["z"] = self.z
 
 		return {'FINISHED'}
 
+	def invoke(self, context, event):
+		bpy.context.view_layer.update()
 
+		self.execute(context)
 
+		return {'FINISHED'}
